4e03dd552255f734d8dde1bd9d95c3a4239c675e
Martin Alexander Neumann <mneumann@teco.edu>
2020-11-11 16:36:33 +0100

    Fix comment that stopped containers are not deleted

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -38,7 +38,7 @@ c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 # volume_driver is no longer a keyword argument to create_container()
 # c.DockerSpawner.extra_create_kwargs.update({ 'volume_driver': 'local' })
-# Remove containers once they are stopped
+# Do not remove containers once they are stopped
 c.DockerSpawner.remove_containers = False
 # For debugging arguments passed to spawned containers
 c.DockerSpawner.debug = True
